fd5e5033e7b2af6126ea7979c9e094e9550dc950
Ngô͘ Hê-bí <shiami@users.noreply.github.com>
2025-04-06 05:08:11 +0800

    Ap-té chu-liāu.

--- assets/javascripts/viewer.js
+++ assets/javascripts/viewer.js
@@ -1,11 +1,11 @@
 (function () {
   // TODO edit
-  const baseUrl = '/scannedImgViewer';
+  const baseUrl = '/1937TaioanChhiubakPianLam';
   const dataFolderPath = '/chuliau';
-  const dataNamePrefix = '/TJTST1_';
+  const dataNamePrefix = '/1937TaioanChhiubakPianLam_Page_';
   const dataNameNumberCount = 3;
   const dataNameSuffix = '.jpg';
-  const lastPageNumber = 12;
+  const lastPageNumber = 346;
 
   let imgSources = [];
   const imgSourcesMin = 1;
